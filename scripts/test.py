5ed1e63aeac9826b919c8ba20eb2fa0f6fefe764
Mohammad Mejbah Ul Alam <alamm1@jneuberg-mobl1.amr.corp.intel.com>
2018-02-26 22:10:23 -0800

    rearranged scripts

--- scripts/test.py
+++
@@ -1,8 +0,0 @@
-from utils import *
-from sys import argv
-
-if __name__ == "__main__" :
-  dataDirGood = argv[1]
-  dataDirBad = argv[2]
-  counterId = argv[3] 
-  print compareCounters(dataDirGood, dataDirBad, counterId)
